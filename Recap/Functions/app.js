bee9174e0e11e71cc7ddd5de108eddca2f22e1f7
ibrahim-sert <evo3506@gmail.com>
2022-12-23 00:55:48 +0300

    exercise added

---
+++ Recap/Functions/app.js
@@ -0,0 +1,57 @@
+// function name(a, b) {
+//   return a * b + a * b * 4;
+// }
+// name(5, 6);
+// console.log(name(5, 6));
+
+// function ciftTekSayilar(num) {
+//   for (i = 0; i <= num; i++) {
+//     if (i % 2 == 0) {
+//       console.log(`${i} is even number`);
+//     } else {
+//       console.log(`${i} is odd numbers`);
+//     }
+//   }
+// }
+// console.log(ciftTekSayilar(20));
+
+// function bölüm(num1) {
+//   for (i = 0; i <= num1; i++)
+//     if (i % 2 === 0 && i % 3 !== 0) {
+//       console.log(`${i} buzz`);
+//     } else if (i % 2 !== 0 && i % 3 === 0) {
+//       console.log(`${i} fizz`);
+//     } else if (i % 2 === 0 && i % 3 === 0) {
+//       console.log(`${i} fizzbuzz`);
+//     }
+// }
+
+// console.log(bölüm(85));
+
+// const yeniNesil = function (a, b, c, d) {
+//   return a * b + 5 + (c + d);
+// };
+// console.log(yeniNesil(1, 2, 3, 4));
+
+//?=======HESAP MAKİNASI==========
+
+// const calculate = function (x, y, op) {
+//   if (op == "+") {
+//     return x + y;
+//   }
+//   if (op == "-") {
+//     return x - y;
+//   }
+//   if (op == "/") {
+//     return x / y;
+//   }
+//   if (op == "*") {
+//     return x * y;
+//   }
+// };
+
+console.log(calculate(8, 3, "*"));
+
+function bölüm(num1){
+    
+}
