63a793d33eca36d7a52a743e7337a522a0e9d5ff
ibrahim-sert <evo3506@gmail.com>
2023-01-02 14:22:44 +0300

    edited

--- safa/js/day5.app.js
+++ safa/js/day5.app.js
@@ -1,2 +1,50 @@
-const numbers = [1, 2, 3, 4, 5]
-let numbers[0] = 10 
+// const numbers = [1, 2, 3, 4, 5]
+// let numbers[0] = 10
+
+// const x =[]
+
+// let y = Array()
+
+// console.log(x);
+// console.log(y);
+
+// const a =new Array()
+// console.log(a);
+// console.log(typeof x);
+// myArr.length
+// Array.isArray(x)
+// let dailyActivities = ['eat', 'work', 'sleep'];
+
+// // add an element at the end
+// const x = dailyActivities.push('exercise');
+// console.log(dailyActivities); //  ['eat', 'work', 'sleep', 'exercise']
+// console.log(typeof dailyActivities.push('exercise')); // 4
+
+// let dailyActivities = [ 'eat', 'sleep', "1", true]
+
+// dailyActivities[2] = 'exercise';
+
+// console.log(dailyActivities);
+
+// var x = ["John", "James"];
+// var y = ["Aaron", "Oliver"];
+// var z = ["John"];
+// var names = x.concat(y).concat(z);
+// console.log(names);
+
+// let arr =  [1, 2, 3, 4];
+
+// let y = arr.reverse()
+
+// console.log(arr);
+// let y = arr.join('')
+// console.log(y);//returns 'abcd'
+// console.log(arr.join('-'));//returns 'a-b-c-d'
+// console.log(arr)
+
+// const cwPaths = ["Full-Stack", "AWS-Devops", "Data Science", "Cyber Security", "Salesforce"];
+
+// const numbers = [0, 1, 111, 2, 3.6, 3, 10, ".", 20, 30];
+// numbers.sort();
+// console.log(numbers);
+// [ 0, 1, 10, 2, 20, 3, 30 ]
